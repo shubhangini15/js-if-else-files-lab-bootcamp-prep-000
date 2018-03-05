f0408b84cb7d89046051e16ff4a585f940dfc4ca
sxp04300@ucmo.edu <sxp04300@ucmo.edu>
2018-03-05 19:24:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var name = "Susan";
-var height = 70;
+var name = "Susan"
+var height = 70
 
 
 // Don't worry about this
-module.exports = { name, height
-}
+module.exports = { name, height }
 
-if (name === "Joe"){
-console.log("The name is correct")
-}else{
-  console.log("Expected the name to be Joe")
-}
